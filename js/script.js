14cb99a2f59b9ad968aac94eda39c2116eae380a
JhonCode <jjv20618@gmail.com>
2024-01-20 22:52:57 -0600

    4to diseño
    He agregado un cuarto diseño y además he agregado esa animación de carga para la página en general

--- js/script.js
+++ js/script.js
@@ -2,17 +2,25 @@ let btnLoader2 = null;
 let barraCarga = null;
 let txtPorcentaje = null;
 
+//Variables que seleccionaran los elementos para el cuerpo de la pagina y la carga que se va a mostrar
+let imgloader = null;
+let contentMain = null;
+
 let anchura = 0;
 
 document.addEventListener('DOMContentLoaded', (evento) => {
     cargarElementosDom();
     cargarEventos();
+    mostrar_ocultarElemento(imgloader);
+    mostrar_ocultarElemento(contentMain);
 });
 
 function cargarElementosDom() {
     btnLoader2 = document.querySelector('.ejecutar-loader2');
     barraCarga = document.querySelectorAll('.loader-2__barra');
     txtPorcentaje = document.querySelectorAll('.porcent-barra');
+    imgloader = document.querySelector('.img-loader');
+    contentMain = document.querySelector('.content-gral');
 }
 
 function cargarEventos(params) {
@@ -58,5 +66,20 @@ function aplicarTxtElement(elemento, texto){
     //Si no, aplicamos al elemento
     } else {
         elemento.style.width = texto;
+    }
+}
+
+//Solo funciona en linea de codigo general, no accede a las hojas de estilo
+function mostrar_ocultarElemento(elemento){
+    try {
+        if (elemento.style.display === 'none') {
+            elemento.style.display = 'block';
+            console.log(`Se ha mostrado el elemento${elemento}`);
+        }else{
+            elemento.style.display = 'none';
+            console.log(`Se ha ocultado el elemento ${elemento}`);
+        }
+    } catch (error) {
+        console.error(`Excepción en el método mostrar_ocultarElemento ${error.message}`);
     }
 }
